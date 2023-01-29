adcbbf681f4e8e85ce6675a518fd001f00fe8bcf
Neil Cummings <31762828+TryCatchLearn@users.noreply.github.com>
2023-01-29 16:26:47 +0700

    End of section 6

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 interface Props {
     darkMode: boolean;
@@ -15,21 +16,24 @@ const midLinks = [
 
 const rightLinks = [
     { title: 'login', path: '/login' },
-    { title: 'register', path: '/register' },
+    { title: 'register', path: '/register' }
 ]
 
-export default function Header({handleThemeChange, darkMode}: Props) {
-    const navLinkStyles = {
-        color: 'inherit',
-        textDecoration: 'none',
-        typography: 'h6',
-        '&:hover': {
-            color: 'grey.500'
-        },
-        '&.active': {
-            color: 'text.secondary'
-        }
+const navLinkStyles = {
+    color: 'inherit',
+    textDecoration: 'none',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
     }
+}
+
+export default function Header({handleThemeChange, darkMode}: Props) {
+    const {basket} = useStoreContext();
+    const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
         <AppBar position='static' sx={{mb:4}}>
@@ -60,8 +64,8 @@ export default function Header({handleThemeChange, darkMode}: Props) {
                 </List>
 
                 <Box display='flex' alignItems='center'>
-                    <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
-                        <Badge badgeContent='4' color='secondary'>
+                    <IconButton component={Link} to='/basket' size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
+                        <Badge badgeContent={itemCount} color='secondary'>
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
